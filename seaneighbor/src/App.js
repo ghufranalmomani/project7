d7d8dea29a1f55b662c7b27ffbadfc5fd1dde606
10-Anas <anasq0q@gmail.com>
2022-07-06 12:25:05 +0300

    change template

---
+++ seaneighbor/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+import Nav from './Components/Nav';
+import Footer from './Components/Footer';
+
+function App() {
+
+return(
+  <>
+<Nav />
+<Footer />
+  </>
+  );
+}
+
+export default App;
