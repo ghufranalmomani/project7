2d0e67e7f4f77ec1ccd3b574d23539be973ae765
14 ghufran almomani <almomanighufran@gmail.com>
2022-07-08 13:37:25 -0600

    project7

---
+++ seaneighbor/src/About.js
@@ -0,0 +1,457 @@
+import React from 'react';
+
+
+function About(){
+    return(
+
+<>
+  {/* Mobile Specific Meta */}
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1, shrink-to-fit=no"
+  />
+  {/* Favicon*/}
+  <link rel="shortcut icon" href="img/fav.png" />
+  {/* Author Meta */}
+  <meta name="author" content="colorlib" />
+  {/* Meta Description */}
+  <meta name="description" content="" />
+  {/* Meta Keyword */}
+  <meta name="keywords" content="" />
+  {/* meta character set */}
+  <meta charSet="UTF-8" />
+  {/* Site Title */}
+  <title>Travel</title>
+  <link
+    href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700"
+    rel="stylesheet"
+  />
+  {/*
+			CSS
+			============================================= */}
+  
+    
+  {/* #header */}
+  {/* start banner Area */}
+  <section className="about-banner relative">
+    <div className="overlay overlay-bg" />
+    <div className="container">
+      <div className="row d-flex align-items-center justify-content-center">
+        <div className="about-content col-lg-12">
+          <h1 className="text-white">About Us</h1>
+          <p className="text-white link-nav">
+            <a href="index.html">Home </a>{" "}
+            <span className="lnr lnr-arrow-right" />{" "}
+            <a href="about.html"> About Us</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  {/* End banner Area */}
+  {/* Start about-info Area */}
+  <section className="about-info-area section-gap">
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col-lg-6 info-left">
+          <img className="img-fluid" src="https://cdn.discordapp.com/attachments/962769580818858015/994507587389247488/2.jpg" alt="" />
+        </div>
+        <div className="col-lg-6 info-right">
+          <h6>About Us</h6>
+          <h1>Who We Are?</h1>
+          <p>
+            Here, I focus on a range of items and features that we use in life
+            without giving them a second thought. such as Coca Cola. Dolor sit
+            amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+            nostrud exercitation ullamco.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  {/* End about-info Area */}
+  {/* Start price Area */}
+  <section className="price-area section-gap">
+    <div className="container">
+      <div className="row d-flex justify-content-center">
+        <div className="menu-content pb-70 col-lg-8">
+          <div className="title text-center">
+            <h1 className="mb-10">We Provide Affordable Prices</h1>
+            <p>
+              Well educated, intellectual people, especially scientists at all
+              times demonstrate considerably.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-4">
+          <div className="single-price">
+            <h4>Cheap Packages</h4>
+            <ul className="price-list">
+              <li className="d-flex justify-content-between align-items-center">
+                <span>New York</span>
+                <a href="#" className="price-btn">
+                  $1500
+                </a>
+              </li>
+              <li className="d-flex justify-content-between align-items-center">
+                <span>Maldives</span>
+                <a href="#" className="price-btn">
+                  $1500
+                </a>
+              </li>
+              <li className="d-flex justify-content-between align-items-center">
+                <span>Sri Lanka</span>
+                <a href="#" className="price-btn">
+                  $1500
+                </a>
+              </li>
+              <li className="d-flex justify-content-between align-items-center">
+                <span>Nepal</span>
+                <a href="#" className="price-btn">
+                  $1500
+                </a>
+              </li>
+              <li className="d-flex justify-content-between align-items-center">
+                <span>Thiland</span>
+                <a href="#" className="price-btn">
+                  $1500
+                </a>
+              </li>
+              <li className="d-flex justify-content-between align-items-center">
+                <span>Singapore</span>
+                <a href="#" className="price-btn">
+                  $1500
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-lg-4">
+          <div className="single-price">
+            <h4>Luxury Packages</h4>
+            <ul className="price-list">
+              <li className="d-flex justify-content-between align-items-center">
+                <span>New York</span>
+                <a href="#" className="price-btn">
+                  $1500
+                </a>
+              </li>
+              <li className="d-flex justify-content-between align-items-center">
+                <span>Maldives</span>
+                <a href="#" className="price-btn">
+                  $1500
+                </a>
+              </li>
+              <li className="d-flex justify-content-between align-items-center">
+                <span>Sri Lanka</span>
+                <a href="#" className="price-btn">
+                  $1500
+                </a>
+              </li>
+              <li className="d-flex justify-content-between align-items-center">
+                <span>Nepal</span>
+                <a href="#" className="price-btn">
+                  $1500
+                </a>
+              </li>
+              <li className="d-flex justify-content-between align-items-center">
+                <span>Thiland</span>
+                <a href="#" className="price-btn">
+                  $1500
+                </a>
+              </li>
+              <li className="d-flex justify-content-between align-items-center">
+                <span>Singapore</span>
+                <a href="#" className="price-btn">
+                  $1500
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-lg-4">
+          <div className="single-price">
+            <h4>Camping Packages</h4>
+            <ul className="price-list">
+              <li className="d-flex justify-content-between align-items-center">
+                <span>New York</span>
+                <a href="#" className="price-btn">
+                  $1500
+                </a>
+              </li>
+              <li className="d-flex justify-content-between align-items-center">
+                <span>Maldives</span>
+                <a href="#" className="price-btn">
+                  $1500
+                </a>
+              </li>
+              <li className="d-flex justify-content-between align-items-center">
+                <span>Sri Lanka</span>
+                <a href="#" className="price-btn">
+                  $1500
+                </a>
+              </li>
+              <li className="d-flex justify-content-between align-items-center">
+                <span>Nepal</span>
+                <a href="#" className="price-btn">
+                  $1500
+                </a>
+              </li>
+              <li className="d-flex justify-content-between align-items-center">
+                <span>Thiland</span>
+                <a href="#" className="price-btn">
+                  $1500
+                </a>
+              </li>
+              <li className="d-flex justify-content-between align-items-center">
+                <span>Singapore</span>
+                <a href="#" className="price-btn">
+                  $1500
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  {/* End price Area */}
+  {/* Start other-issue Area */}
+  <section className="other-issue-area section-gap">
+    <div className="container">
+      <div className="row d-flex justify-content-center">
+        <div className="menu-content pb-70 col-lg-9">
+          <div className="title text-center">
+            <h1 className="mb-10">Other issues we can help you with</h1>
+            <p>
+              We all live in an age that belongs to the young at heart. Life
+              that is.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-3 col-md-6">
+          <div className="single-other-issue">
+            <div className="thumb">
+              <img className="img-fluid" src="https://cdn.discordapp.com/attachments/962769580818858015/994507750291816468/13.jpg" alt="" />
+            </div>
+            <a href="#">
+              <h4>Rent a Car</h4>
+            </a>
+            <p>
+              The preservation of human life is the ultimate value, a pillar of
+              ethics and the foundation.
+            </p>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-6">
+          <div className="single-other-issue">
+            <div className="thumb">
+              <img className="img-fluid" src="https://cdn.discordapp.com/attachments/962769580818858015/994507749826236476/11.jpg" alt="" />
+            </div>
+            <a href="#">
+              <h4>Cruise Booking</h4>
+            </a>
+            <p>
+              I was always somebody who felt quite sorry for myself, what I had
+              not got compared.
+            </p>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-6">
+          <div className="single-other-issue">
+            <div className="thumb">
+              <img className="img-fluid" src="https://cdn.discordapp.com/attachments/962769580818858015/994507750535073792/14.jpg" alt="" />
+            </div>
+            <a href="#">
+              <h4>To Do List</h4>
+            </a>
+            <p>
+              The following article covers a topic that has recently moved to
+              center stage–at least it seems.
+            </p>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-6">
+          <div className="single-other-issue">
+            <div className="thumb">
+              <img className="img-fluid" src="https://cdn.discordapp.com/attachments/962769580818858015/994507750056919111/12.jpg" alt="" />
+            </div>
+            <a href="#">
+              <h4>Food Features</h4>
+            </a>
+            <p>
+              There are many kinds of narratives and organizing principles.
+              Science is driven by evidence.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  {/* End other-issue Area */}
+  <section id="team">
+  <div className="container my-3 py-5 text-center">
+    <div className="row mb-5">
+      <div className="col">
+        <h1>Our Team</h1>
+        <p className="my-3">
+          This team page has a quirky, vibrant energy that immediately catches
+          your attention – a good sign for a design company.
+        </p>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-lg-3 col-md-6 pt-1">
+        <div className="card h-100">
+          <div className="card-body">
+            <img
+              className="img-fouild rounded w-75 mb-3"
+              src="https://avatars.githubusercontent.com/u/100237201?v=4"
+              alt="Sophie"
+            />
+            <h4>Hadi AL-Saden</h4>
+            <h5>Software Engineer</h5>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi,
+              recusandae.
+            </p>
+            <div className="d-flex flex-row justify-content-center">
+              <div className="p-4">
+                <a href="#">
+                <i class="fa-brands fa-facebook"></i>
+                </a>
+              </div>
+              <div className="p-4">
+                <a href="#">
+                  <i className="fab fa-twitter" />
+                </a>
+              </div>
+              <div className="p-4">
+                <a href="#">
+                  <i className="fab fa-instagram" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-3 col-md-6 pt-1">
+        <div className="card h-100">
+          <div className="card-body">
+            <img
+              className="img-fouild rounded w-75 mb-3"
+              src="https://avatars.githubusercontent.com/u/100368515?v=4"
+              alt="lucy"
+            />
+            <h4>Ghufran Almomani</h4>
+            <h5>Product Owner</h5>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi,
+              recusandae.
+            </p>
+            <div className="d-flex flex-row justify-content-center">
+              <div className="p-4">
+                <a href="#">
+                <i class="fa-brands fa-facebook"></i>
+                </a>
+              </div>
+              <div className="p-4">
+                <a href="#">
+                  <i className="fab fa-twitter" />
+                </a>
+              </div>
+              <div className="p-4">
+                <a href="#">
+                  <i className="fab fa-instagram" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-3 col-md-6 pt-1">
+        <div className="card h-100">
+          <div className="card-body">
+            <img
+              className="img-fouild rounded w-75 mb-3"
+              src="https://avatars.githubusercontent.com/u/100212231?v=4"
+              alt="Brad"
+            />
+            <h4>Anas Allawafeh</h4>
+            <h5>Scrum Master</h5>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi,
+              recusandae.
+            </p>
+            <div className="d-flex flex-row justify-content-center">
+              <div className="p-4">
+                <a href="#">
+                <i class="fa-brands fa-facebook"></i>
+                </a>
+              </div>
+              <div className="p-4">
+                <a href="#">
+                  <i className="fab fa-twitter" />
+                </a>
+              </div>
+              <div className="p-4">
+                <a href="#">
+                  <i className="fab fa-instagram" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-3 col-md-6 pt-1">
+        <div className="card h-100">
+          <div className="card-body">
+            <img
+              className="img-fouild rounded w-75 mb-3"
+              src="https://avatars.githubusercontent.com/u/100063786?v=4"
+              alt="andres"
+            />
+            <h4>Ro'a Yaseen</h4>
+            <h5>Software Engineer</h5>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi,
+              recusandae.
+            </p>
+            <div className="d-flex flex-row justify-content-center">
+              <div className="p-4">
+                <a href="#">
+                <i class="fa-brands fa-facebook"></i>
+                </a>
+              </div>
+              <div className="p-4">
+                <a href="#">
+                  <i className="fab fa-twitter" />
+                </a>
+              </div>
+              <div className="p-4">
+                <a href="#">
+                  <i className="fab fa-instagram" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+</>
+
+
+
+        )
+    }
+    export default About;
