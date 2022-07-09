fe86ae805469da3e92fb53da83df62e2d1b95e35
Hadi-AL-Saden <hadialsaden@gmail.com>
2022-07-08 20:51:48 -0800

    Signup-done

---
+++ seaneighbor/php/index.php
@@ -0,0 +1,65 @@
+<?php
+
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: access');
+header('Access-Control-Allow-Methods: Post');
+header('Content-Type: application/json; charset=UTF-8');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With ');
+
+
+
+
+$data = json_decode(file_get_contents("php://input"));
+
+$name = $data->name;
+$phone = $data->phone;
+$email = $data->email;
+$password = $data->password;
+
+// $con = mysqli_connect("localhost:3000","root","project-7" ,"");
+// mysqli_select_db($con,"users");
+
+
+
+
+$servername = "localhost";
+$username   = "root";
+$passsword   = "";
+$dbname     = "project-7";
+// Create connection
+$con = new mysqli($servername, $username, $passsword, $dbname);
+
+
+
+
+
+
+
+$sql = "INSERT INTO users(
+    name,
+    phone,
+    email,
+    password
+)
+VALUES(
+    '$name',
+    '$phone',
+    '$email',
+    '$password'
+    
+)";
+
+$result = mysqli_query($con, $sql);
+if ($result) {
+    $response['data'] = array(
+        'status' => 'valid'
+    );
+    echo json_encode($response);
+} else {
+    $response['data'] = array(
+        'status' => 'invalid'
+    );
+    echo json_encode($response);
+}
+?>
---
+++ seaneighbor/src/Signup/Signup.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import './Signup.css';
+import axios from "axios";
+import {useNavigate} from 'react-router-dom'
+
+
+const Signup=(props)=>{
+
+    let history = useNavigate();
+    const [data, setData]=useState({
+        name:'',
+        email:'',
+        phone:'',
+        password:''
+        
+    })  
+    const handleChange=(e)=>{
+        setData({...data, [e.target.name]: e.target.value});
+    }
+
+const submitForm=(e)=>{
+    e.preventDefault();
+
+const sendData={
+        name:data.name,
+        email:data.email,
+        phone:data.phone,
+        password:data.password
+        
+    }
+console.log(sendData,"data")
+axios.post('http://localhost/project-7-db/',sendData)
+.then((result)=>{
+    if(result.data.Status == 'Invalid'){
+         alert('Invalid  User');
+    }
+   
+    else {
+        history(`./Login`);
+    }
+})
+
+}
+
+
+
+
+    return(
+
+
+<div>
+
+<div className="ss">
+<form action="" className="form" onSubmit={submitForm}>
+
+    <label htmlFor="name" className="form-label">Name:</label>
+<input type="text" name="name" className="form-control"  onChange={handleChange} value={data.name} required />
+
+    <label htmlFor="email" className="form-label">Email:</label>
+<input type="eamil" name="email" className="form-control"  onChange={handleChange} value={data.email}  required />
+
+    <label htmlFor="phone">Phone:</label>
+<input type="text" name="phone"  className="form-control"  onChange={handleChange} value={data.phone}  required />
+
+    <label htmlFor="password">Password:</label>
+<input type="password" name="password"   className="form-control"  onChange={handleChange} value={data.password} required />
+
+    <label htmlFor="Confirm">Confirm password:</label>
+<input type="password" name="Confirm"  className="btn btn-outline-secondary"   />
+
+<br/>
+<br/>
+
+<input type="submit" name="Submit" className="btn btn-outline-warning" />
+
+
+</form>
+
+
+</div>
+
+
+
+
+
+</div>
+
+
+
+    
+
+    )
+};
+export default Signup;
