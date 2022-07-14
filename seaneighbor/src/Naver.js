8b2e7e4ac822cd00fc02cc84cfb332f4092caf00
10-Anas <anasq0q@gmail.com>
2022-07-14 16:17:20 +0300

    fix merge error

--- seaneighbor/src/Naver.js
+++ seaneighbor/src/Naver.js
@@ -1,18 +1,10 @@
-
-import React, {useState} from 'react';
-
 import React, {useEffect, useState} from 'react';
-
-
 import Main from './main';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Signup from './Signup/Signup';
 import Button from 'react-bootstrap/Button';
-
-
-
 import Messege from './Components/message';
 import { NavLink,Link,useLocation  } from 'react-router-dom'
 
